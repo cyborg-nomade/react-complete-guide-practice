26a9254b41a38f430b54864e5026a2f2cf2934a0
Uriel Fiori <uriel.fiori@gmail.com>
2021-11-16 16:01:32 -0300

    composition (children props)

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,4 +1,6 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+
 import "./Expenses.css";
 
 import React from "react";
@@ -7,7 +9,7 @@ function Expenses(props) {
   const expenses = props.expenses;
 
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
@@ -28,7 +30,7 @@ function Expenses(props) {
         amount={expenses[3].amount}
         date={expenses[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
