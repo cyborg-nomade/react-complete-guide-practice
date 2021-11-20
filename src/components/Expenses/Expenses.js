6e9c735d141c7121bd63c6a35fe708e3a80b0e32
Uriel Fiori <uriel.fiori@gmail.com>
2021-11-20 16:36:26 -0300

    rendering lists of data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,6 @@ import "./Expenses.css";
 import React, { useState } from "react";
 
 const Expenses = (props) => {
-  const expenses = props.expenses;
   const [selectedYear, setSelectedYear] = useState("2020");
 
   const filterSelectedHandler = (filter) => {
@@ -19,26 +18,13 @@ const Expenses = (props) => {
           selectedYear={selectedYear}
           onFilterSelected={filterSelectedHandler}
         />
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        />
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
-        <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        />
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
