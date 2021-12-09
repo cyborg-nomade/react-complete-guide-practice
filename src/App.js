4792f516d1aaded6662bc922767051553fa03ffa
Uriel Fiori <uriel.fiori@gmail.com>
2021-12-09 12:49:09 -0300

    adding a header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from "./components/Layout/Header";
+import { Fragment } from "react";
+
 function App() {
   return (
-    <div>
+    <Fragment>
+      <Header />
       <h2>Let's get started!</h2>
-    </div>
+    </Fragment>
   );
 }
 
