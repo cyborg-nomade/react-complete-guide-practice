e846bb044c1ec737fa32bbdaae2e442a67b027cb
Uriel Fiori <uriel.fiori@gmail.com>
2022-01-18 14:54:51 -0300

    splitting our code

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
-import { counterActions } from "./../store/index";
+import { counterActions } from "./../store/counter";
 
 const Counter = () => {
   const dispatch = useDispatch();
@@ -9,7 +9,7 @@ const Counter = () => {
   const showCounter = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
-    dispatch(counterActions.increase());
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
