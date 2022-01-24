87c6ef360d3c4971333cb1bd7a629cf8727dcfdc
Uriel Fiori <uriel.fiori@gmail.com>
2022-01-24 14:26:15 -0300

    practice redirecting and extracting params

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import QuoteDetails from "./pages/QuoteDetails";
 import NewQuote from "./pages/NewQuote";
@@ -6,6 +6,9 @@ import NewQuote from "./pages/NewQuote";
 function App() {
   return (
     <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
       <Route path="/quotes" component={AllQuotes} exact />
       <Route path="/quotes/:quoteId" component={QuoteDetails} />
       <Route path="/new-quote" component={NewQuote} />
--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const QuoteDetails = (props) => {
-  return <h1>Quote Details Page</h1>;
+  const params = useParams();
+  return (
+    <React.Fragment>
+      <h1>Quote Details Page</h1>
+      <p>{params.quoteId}</p>
+    </React.Fragment>
+  );
 };
 
 export default QuoteDetails;
