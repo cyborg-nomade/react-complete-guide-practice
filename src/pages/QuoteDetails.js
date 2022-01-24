e8db67516f76130d7688fc8ca6829a0bb8ad5cb2
Uriel Fiori <uriel.fiori@gmail.com>
2022-01-24 18:00:24 -0300

    outputting data on the details page

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,13 +1,38 @@
 import React from "react";
 import { useParams, Route } from "react-router-dom";
 import Comments from "./../components/comments/Comments";
+import HighlightedQuote from "./../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Max",
+    text: "This is really a quote",
+  },
+  {
+    id: "q2",
+    author: "Uriel",
+    text: "Realy!?",
+  },
+  {
+    id: "q3",
+    author: "John",
+    text: "Impressive skills at quotation!",
+  },
+];
 
 const QuoteDetails = (props) => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <React.Fragment>
-      <h1>Quote Details Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`} component={Comments} />
     </React.Fragment>
   );
