996f2c3801a044385ac9543484d3243aeb817c19
Uriel Fiori <uriel.fiori@gmail.com>
2022-01-24 17:53:13 -0300

    adding dummy data and more content

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,26 @@
 import React from "react";
+import QuoteList from "./../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Max",
+    text: "This is really a quote",
+  },
+  {
+    id: "q2",
+    author: "Uriel",
+    text: "Realy!?",
+  },
+  {
+    id: "q3",
+    author: "John",
+    text: "Impressive skills at quotation!",
+  },
+];
 
 const AllQuotes = (props) => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
