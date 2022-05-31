2d7073b36440c0281dd2eaaa2e2304db3e6e729a
Uriel Fiori <uriel.fiori@gmail.com>
2022-05-31 19:30:01 -0300

    outputting a list of meetups

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import MeetupList from "./../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/c/c2/001_Jundia%C3%AD.jpg",
+    address: "Some Adress, 123, Some City",
+    description: "This is a test",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/c/c2/001_Jundia%C3%AD.jpg",
+    address: "Other Adress, 123, Some City",
+    description: "This is another test",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default HomePage;
