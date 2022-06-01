c5c0d07b2a9c91fdeb6d1622ccb155d93da24951
Uriel Fiori <uriel.fiori@gmail.com>
2022-06-01 11:34:10 -0300

    adding custom components and CSS module

---
+++ pages/[meetUpId]/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import MeetupDetail from "./../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/c/c2/001_Jundia%C3%AD.jpg"
+      title="A First Meetup"
+      address="Some City 5, Some Street"
+      description="A meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
