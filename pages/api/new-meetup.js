eac5ec8390e6d65047e0125fc0a53a8ab44fdbce
Uriel Fiori <uriel.fiori@gmail.com>
2022-06-01 15:13:18 -0300

    working with mongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,12 +1,27 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 
 // POST /api/new-meetup
 
-const handler = (req, res) => {
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://next-api:this-is-a-test@cluster0.x8t1a.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollections = db.collection("meetups");
+
+    const result = await meetupsCollections.insertOne({ data });
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 };
 
