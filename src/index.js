02dfc86962725e9827a113c4eb4698cbc501ed21
Uriel Fiori <uriel.fiori@gmail.com>
2022-06-02 14:29:42 -0300

    new project setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import "./index.css";
-import App from "./App";
-import { AuthContextProvider } from "./store/auth-context";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <AuthContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </AuthContextProvider>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
